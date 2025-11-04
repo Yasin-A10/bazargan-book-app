ab16ac03fc746c1bad44d12c0e649d3be5af4d36
Yasin-A10 <yasinzizi8383@gmail.com>
2025-11-04 19:05:29 +0330

    add modal to audio book page

--- lib/features/book/presentation/screen/audio_book_screen.dart
+++ lib/features/book/presentation/screen/audio_book_screen.dart
@@ -4,6 +4,10 @@ import 'package:bazargan/core/blocs/audio/audio_bloc.dart';
 import 'package:bazargan/core/constants/colors.dart';
 import 'package:bazargan/core/constants/texts.dart';
 import 'package:bazargan/core/utils/number_formater.dart';
+import 'package:bazargan/core/widgets/button/button.dart';
+import 'package:bazargan/core/widgets/inputs/star_rating.dart';
+import 'package:bazargan/core/widgets/inputs/text_form_field.dart';
+import 'package:bazargan/core/widgets/list_item_widget.dart';
 import 'package:bazargan/features/book/data/model/audio_book_model.dart';
 import 'package:bazargan/features/book/presentation/bloc/audio_book_bloc/audio_book_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -33,6 +37,8 @@ class _AudioBookScreenState extends State<AudioBookScreen> {
   int _currentIndex = 0;
 
   final List<double> _speeds = [1, 1.25, 1.5, 1.75, 2];
+
+  AudioBookModel? _book;
 
   @override
   void initState() {
@@ -254,6 +260,18 @@ class _AudioBookScreenState extends State<AudioBookScreen> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Iconsax.more_copy,
+              color: AppColors.neutral353535,
+              size: 16,
+            ),
+            onPressed: () {
+              _openMenu(context, _book!);
+            },
+          ),
+        ],
         shape: LinearBorder.bottom(
           side: BorderSide(
             color: !_isScrolled ? Colors.transparent : AppColors.neutralEDEDED,
@@ -280,6 +298,7 @@ class _AudioBookScreenState extends State<AudioBookScreen> {
           if (state is AudioBookLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is AudioBookSuccess) {
+            _book = state.audioBooks.first;
             if (_audioBooks.isEmpty ||
                 _audioBooks.first.id != state.audioBooks.first.id) {
               _audioBooks = state.audioBooks;
@@ -648,6 +667,294 @@ class _AudioBookScreenState extends State<AudioBookScreen> {
           }
         },
       ),
+    );
+  }
+
+  void _openMenu(BuildContext context, AudioBookModel book) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Stack(
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.of(context).pop(),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    transform: GradientRotation(0.4),
+                    colors: [
+                      AppColors.primary.withValues(alpha: 0.4),
+                      AppColors.neutralMidnight.withValues(alpha: 0.2),
+                      AppColors.secondary.withValues(alpha: 0.4),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Material(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                    bottom: 16,
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Divider(
+                        color: AppColors.neutral757575,
+                        thickness: 3,
+                        endIndent: 140,
+                        indent: 140,
+                      ),
+                      const SizedBox(height: 16),
+                      Divider(color: AppColors.neutralE3E3E3, thickness: 1),
+                      const SizedBox(height: 8),
+                      ListItemWidget(
+                        title: 'صفحه معرفی کتاب',
+                        titleStyle: AppTextStyles.body.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        rightIcon: Icon(
+                          Iconsax.book_copy,
+                          size: 20,
+                          color: AppColors.neutralMidnight,
+                        ),
+                        leftIcon: Iconsax.arrow_left_2_copy,
+                        onPressed: () {
+                          context.push('/book/${book.parentBookId}');
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      Divider(color: AppColors.neutralE3E3E3, thickness: 1),
+                      const SizedBox(height: 8),
+                      ListItemWidget(
+                        title: 'فهرست کتاب',
+                        titleStyle: AppTextStyles.body.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        rightIcon: Icon(
+                          Iconsax.menu_1_copy,
+                          size: 20,
+                          color: AppColors.neutralMidnight,
+                        ),
+                        leftIcon: Iconsax.arrow_left_2_copy,
+                        onPressed: () {
+                          // _openCategoryMenu(context, book);
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      Divider(color: AppColors.neutralE3E3E3, thickness: 1),
+                      const SizedBox(height: 8),
+                      ListItemWidget(
+                        title: 'افزودن نظر',
+                        titleStyle: AppTextStyles.body.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        rightIcon: Icon(
+                          Iconsax.messages_3_copy,
+                          size: 20,
+                          color: AppColors.neutralMidnight,
+                        ),
+                        leftIcon: Iconsax.arrow_left_2_copy,
+                        onPressed: () {
+                          _openReviewMenu(context, book.name!);
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      Divider(color: AppColors.neutralE3E3E3, thickness: 1),
+
+                      const SizedBox(height: 8),
+                      ListItemWidget(
+                        title: 'حذف از کتابخانه من',
+                        titleStyle: AppTextStyles.body.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        rightIcon: Icon(
+                          Iconsax.trash_copy,
+                          size: 20,
+                          color: AppColors.neutralMidnight,
+                        ),
+                        onPressed: () {
+                          _openDeleteMenu(context);
+                        },
+                      ),
+
+                      const SizedBox(height: 8),
+                      Divider(color: AppColors.neutralE3E3E3, thickness: 1),
+                      const SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _openReviewMenu(BuildContext context, String bookName) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.white,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 16,
+            right: 16,
+            top: 8,
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 16,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Divider(
+                  color: AppColors.neutral757575,
+                  thickness: 3,
+                  endIndent: 140,
+                  indent: 140,
+                ),
+                Text(
+                  'ثبت نظر',
+                  style: AppTextStyles.headlineLarge.copyWith(
+                    color: AppColors.primary,
+                  ),
+                ),
+                const SizedBox(height: 0),
+                Column(
+                  spacing: 8,
+                  children: [
+                    Text(
+                      bookName,
+                      style: AppTextStyles.body.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    StarRating(
+                      maxRating: 5,
+                      onRatingChanged: (rating) {
+                        //? Handle rating change
+                      },
+                    ),
+                  ],
+                ),
+                InputTextFormField(
+                  label: 'متن نظر',
+                  keyboardType: TextInputType.text,
+                  maxLines: 6,
+                  controller: TextEditingController(),
+                ),
+                Button(
+                  label: 'افزودن نظر',
+                  onPressed: () {
+                    context.pop();
+                  },
+                  width: double.infinity,
+                  backgroundColor: AppColors.secondary,
+                  textColor: AppColors.white,
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _openDeleteMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.only(
+            top: 8,
+            bottom: 20,
+            left: 16,
+            right: 16,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            spacing: 16,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Divider(
+                color: AppColors.neutral757575,
+                thickness: 3,
+                endIndent: 140,
+                indent: 140,
+              ),
+              Text(
+                'حذف کتاب از کتابخانه من',
+                style: AppTextStyles.headlineLarge.copyWith(
+                  color: AppColors.primary,
+                ),
+              ),
+              Text(
+                'امکان دوباره افزودن به کتابخانه من بصورت رایگان وجود دارد',
+                style: AppTextStyles.small.copyWith(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 12,
+                ),
+              ),
+              Row(
+                spacing: 16,
+                children: [
+                  Expanded(
+                    child: Button(
+                      label: 'بازگشت',
+                      onPressed: () {
+                        context.pop();
+                      },
+                      width: double.infinity,
+                      backgroundColor: AppColors.white,
+                      textColor: AppColors.secondary,
+                      borderColor: AppColors.secondary,
+                    ),
+                  ),
+                  Expanded(
+                    child: Button(
+                      label: 'حذف',
+                      onPressed: () {
+                        context.pop();
+                      },
+                      width: double.infinity,
+                      backgroundColor: AppColors.primary,
+                      textColor: AppColors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
